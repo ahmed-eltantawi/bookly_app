020b55e11068a242783929c0163f8ee9837feb3e
Ahmed Eltantawi <ahmedeltantawi73@gmail.com>
2026-04-10 19:25:45 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:bookly_app/features/splash/presentation/manager/views/SplashView.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const BooklyApp());
+}
+
+class BooklyApp extends StatelessWidget {
+  const BooklyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: SplashView());
+  }
+}
