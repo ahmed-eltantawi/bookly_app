d3ce63016566a05748cdbde2e490c5a9ed1d7a0f
Ahmed Eltantawi <ahmedeltantawi73@gmail.com>
2026-04-11 19:18:46 +0200

    add test image

--- lib/core/utils/images_assets.dart
+++ lib/core/utils/images_assets.dart
@@ -1,3 +1,4 @@
 class Assets {
   static const logo = "assets/images/Logo.svg";
+  static const book1 = "assets/images/book1.jpg";
 }
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,10 +1,33 @@
+import 'package:bookly_app/core/utils/images_assets.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return CustomListView();
+  }
+}
+
+class CustomListView extends StatelessWidget {
+  const CustomListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.sizeOf(context).height * .3,
+      child: AspectRatio(
+        aspectRatio: 2 / 3,
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(image: AssetImage(Assets.book1)),
+            color: Colors.red,
+            borderRadius: .circular(9),
+          ),
+        ),
+      ),
+    );
   }
 }
