92e5a748befa39a8aacf7aca9d081ff60fc3bc98
Ahmed Eltantawi <ahmedeltantawi73@gmail.com>
2026-04-18 06:25:13 +0200

    implement local data source method

--- lib/features/home/data/data_sources/home_local_data_source.dart
+++ lib/features/home/data/data_sources/home_local_data_source.dart
@@ -1,6 +1,22 @@
+import 'package:bookly_app/core/utils/constants.dart';
 import 'package:bookly_app/features/home/domain/entities/book_entitiy.dart';
+import 'package:hive/hive.dart';
 
 abstract class HomeLocalDataSource {
   List<BookEntity> fetchNewsBooks();
   List<BookEntity> fetchBestSellerBooks();
+}
+
+class HomeLocalDataSourceImpl extends HomeLocalDataSource {
+  @override
+  List<BookEntity> fetchBestSellerBooks() {
+    var box = Hive.box<BookEntity>(kFeatureBox);
+    return box.values.toList();
+  }
+
+  @override
+  List<BookEntity> fetchNewsBooks() {
+    // TODO: implement fetchNewsBooks
+    throw UnimplementedError();
+  }
 }
